a43ad0dca98149a7e0139b861f526aec49718241
stonedmonkey <90030565+scoobydoge@users.noreply.github.com>
2021-10-22 16:58:57 +0800

    Test run 3

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
     public static void main(String args[]){
         System.out.print("Hello World!");
         System.out.print("Welcome!");
+        System.out.print("Christmas is coming!");
     }
 }
